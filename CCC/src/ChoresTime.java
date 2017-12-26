fa7dd80c2a1d88f68ecd1235981a51738b251d6c
Sentinel7 <amjbvic@gmail.com>
2017-12-25 19:13:41 -0800

    first

---
+++ CCC/src/ChoresTime.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ChoresTime {
+
+	public static void main(String[] args) {
+		
+		Scanner console = new Scanner(System.in);
+		
+		int T = console.nextInt();
+		int C = console.nextInt();
+		int[] mins = new int[C];
+		int result = 0;
+		
+		for (int i=0; i < C; i++) {
+			mins[i] = console.nextInt();
+		}
+		Arrays.sort(mins);
+		
+		for (int i=0; i < C; i++) {
+			if (mins[i] <= T) {
+				result++;
+				T -= mins[i];
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
